4de8ee439991762936f14cd8b49438660099c18a
Shimpi Rajawat <shimpirajawat360@gmail.com>
2026-04-04 00:08:55 +0530

    Fixed get other users route

--- frontend/src/customHooks/getOtherUsers.jsx
+++ frontend/src/customHooks/getOtherUsers.jsx
@@ -1,23 +1,37 @@
-import axios from "axios"
-import { useEffect } from "react"
-import { serverUrl } from "../main"
-import { useDispatch, useSelector } from "react-redux"
-import { setOtherUsers, setUserData } from "../redux/userSlice"
+import axios from "axios";
+import { useEffect } from "react";
+import { serverUrl } from "../main";
+import { useDispatch, useSelector } from "react-redux";
+import { setOtherUsers } from "../redux/userSlice";
 
-const getOtherUsers=()=>{
-    let dispatch=useDispatch()
-    let {userData}=useSelector(state=>state.user)
-    useEffect(()=>{
-        const fetchUser=async ()=>{
-            try {
-                let result=await axios.get(`${serverUrl}/api/user/others`,{withCredentials:true})
-                dispatch(setOtherUsers(result.data))
-            } catch (error) {
-                console.log(error)
-            }
-        }
-        fetchUser()
-    },[userData])
-}
+const getOtherUsers = () => {
+  const dispatch = useDispatch();
+  const { userData } = useSelector((state) => state.user);
 
-export default getOtherUsers
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const token = localStorage.getItem("token"); 
+
+        if (!token) return; 
+
+        const result = await axios.get(
+          `${serverUrl}/api/user/others`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`, 
+            },
+          }
+        );
+
+        dispatch(setOtherUsers(result.data));
+      } catch (error) {
+        console.log("OTHER USERS ERROR:", error.response?.data || error.message);
+      }
+    };
+
+    fetchUser();
+  }, [userData, dispatch]);
+};
+
+export default getOtherUsers;
