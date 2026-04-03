22bf8a9baac171a9115b42a62163c935daebbcfb
Shimpi Rajawat <shimpirajawat360@gmail.com>
2026-04-03 23:47:03 +0530

    Fixed auth response with token

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -31,43 +31,40 @@ res.cookie("token",token,{
     secure:false
    })
 
-   return res.status(201).json(user)
-
+   return res.status(201).json({
+    user,
+    token
+  })
 
    } catch (error) {
     return res.status(500).json({message:`signup error ${error}`})
    } 
 }
-export const login=async (req,res)=>{
+export const login = async (req, res) => {
     try {
-     const {email,password}=req.body
-     const user=await User.findOne({email})
-     if(!user){
-         return res.status(400).json({message:"user does not exist"})
-     }
-
- const isMatch=await bcrypt.compare(password,user.password)
- if(!isMatch){
-    return res.status(400).json({message:"incorrect password"})
- }
- 
- const token=await genToken(user._id)
- 
- res.cookie("token",token,{
-     httpOnly:true,
-     maxAge:7*24*60*60*1000,
-     sameSite:"Strict",
-     secure:false
-    })
- 
-    return res.status(200).json(user)
- 
- 
+      const { email, password } = req.body;
+  
+      const user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).json({ message: "user does not exist" });
+      }
+  
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(400).json({ message: "incorrect password" });
+      }
+  
+      const token = await genToken(user._id);
+  
+      return res.status(200).json({
+        user,
+        token,
+      });
+  
     } catch (error) {
-     return res.status(500).json({message:`login error ${error}`})
-    } 
- }
-
+      return res.status(500).json({ message: `login error ${error}` });
+    }
+  };
  export const logOut=async (req,res)=>{
     try {
         res.clearCookie("token")
